8168d4a0d694510c580a1a18a6334745042c1a05
Samarthya2912 <samarthya55@gmail.com>
2022-08-18 01:11:26 +0530

    Top banner styling updates. Image format changed to SVG.

--- src/components/TopBanner.js
+++ src/components/TopBanner.js
@@ -9,7 +9,7 @@ const TopBanner = () => {
         alignItems: "center",
         backgroundColor: "rgba(11, 110, 79, 1)",
         color: "white",
-        padding: "20px 0"
+        padding: "30px 0"
     }}>
         <div style={{
             display: "flex",
@@ -18,15 +18,15 @@ const TopBanner = () => {
             alignItems: "flex-start",
         }}>
             <h2 style={{ padding: 0, margin: 0 }}>Welcome to</h2>
-            <h1 style={{ padding: 0, margin: "10px 0" }}>AgriAbhyaas</h1>
+            <h1 style={{ padding: 0, margin: "15px 0" }}>AgriAbhyaas</h1>
             <p  style={{ padding: 0, margin: 0, display: "flex", flexDirection: "column", alignItems: "flex-start" }}>
             <span>As they say, practice makes you perfect.</span>
             <span>AgriAbhyaas is your one stop platform to practice
             and excel in challenging subjects.</span>
             </p>
         </div>
-        <img src="assets/tb_image.png" alt="" style={{
-            width: "30%"
+        <img src="assets/top_banner_image.svg" alt="" style={{
+            width: "35%"
         }} />
     </header>
   )
---
+++ src/data/subjects.js
@@ -0,0 +1,8 @@
+const subjects = [
+    {
+        image: "",
+        title: "",
+    }
+];
+
+export default subjects;
