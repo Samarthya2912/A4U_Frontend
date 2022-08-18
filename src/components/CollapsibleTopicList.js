7af02f0e1f1a6cfedbd7bf3f297b85155f6ee8a0
Samarthya2912 <samarthya55@gmail.com>
2022-08-18 23:44:58 +0530

    Collapsible Topic List styling.

--- src/components/CollapsibleTopicList.js
+++ src/components/CollapsibleTopicList.js
@@ -1,16 +1,19 @@
 import React from 'react'
 import { useState } from 'react'
 import topics from '../data/topics_list'
+import ChevronIcon from './ChevronIcon'
 
 const CollapsibleTopicList = () => {
   return (
     <ul style={{
       border: "solid black 1px",
-      maxWidth: "20vw"
+      maxWidth: "20vw",
+      fontSize: "xx-large",
+      fontFamily: "Roboto"
     }}>
-        {topics.map((topic, index) => 
-          <ListItem key={index} {...topic} default={index === 0} />
-        )}
+      {topics.map((topic, index) =>
+        <ListItem key={index} {...topic} default={index === 0} />
+      )}
     </ul>
   )
 }
@@ -26,18 +29,26 @@ const ListItem = (props) => {
     <li style={{
       display: "flex",
       flexDirection: "column",
-      alignItems: "flex-start"
+      alignItems: "flex-start",
+      margin: "10px 0"
     }}>
-        <span>{props.title}<button onClick={collapseHandler}>Click</button></span>
-        <ul style={{
-          height: `${visible? "auto":"0"}`,
-          overflow: "hidden",
-          // transition: "height 0.4s ease"
-        }}>
-            {props.subtopics.map((subtopic, index) => {
-                return <li key={index}>{subtopic}</li>
-            })}
-        </ul>
+      <button style={{
+        background: "transparent",
+        border: "none",
+        cursor: "pointer",
+        fontSize: "large"
+      }} onClick={collapseHandler}><span style={{
+        color: visible ? "dodgerblue" : "black",
+      }}>{props.title} {props.subtopics.length ? <ChevronIcon up={visible} /> : ""}</span></button>
+      <ul style={{
+        height: visible ? "auto" : "0",
+        overflow: "hidden",
+        fontSize: "large"
+      }}>
+        {props.subtopics.map((subtopic, index) => {
+          return <li key={index}>{subtopic}</li>
+        })}
+      </ul>
     </li>
   )
 }
