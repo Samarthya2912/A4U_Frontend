b3ee2692c851d80a77daf21ccef9cce1da92bc53
Samarthya2912 <samarthya55@gmail.com>
2022-08-20 13:24:04 +0530

    OtherPackages slider completed.

--- src/components/OtherPackages.js
+++ src/components/OtherPackages.js
@@ -3,6 +3,28 @@ import React from 'react'
 import other_packages from '../data/other_packages'
 import Check from './shared/Check'
 import StarRating from './shared/StarRating'
+import { useState } from "react"
+
+const Slider = (props) => {
+  const prevSlide = () => {
+    let newindex = (props.showIndex-1)%(props.other_packages.length);
+    if(newindex < 0) newindex += props.other_packages.length;
+
+    props.setShowIndex(newindex);
+  }
+  
+  const nextSlide = () => {
+    props.setShowIndex((props.showIndex+1)%(props.other_packages.length));
+  }
+
+  return <div className="slider">
+    <button onClick={prevSlide} className="slider__slide-btn"><i style={{ fontSize: "x-large" }} className="bi bi-chevron-left"></i></button>
+    {
+      props.other_packages.map((pkg, indx) => <div className={`slider__dot ${props.showIndex === indx? "active":""}`} key={indx}></div>)
+    }
+    <button onClick={nextSlide} className="slider__slide-btn"><i style={{ fontSize: "x-large" }} className="bi bi-chevron-right"></i></button>
+  </div>
+}
 
 const Card = (props) => {
     return (
@@ -25,17 +47,20 @@ const Card = (props) => {
               </div>
               <Button backgroundColor="rgba(19, 174, 126, 1)">View Package</Button>
             </div>
+            <Slider showIndex={props.showIndex} setShowIndex={props.setShowIndex} other_packages={props.other_packages} />
         </div>
     )
 }
 
+
+
 const OtherPackages = () => {
+  const [showIndex, setShowIndex] = useState(0);
+
   return (
     <div className="other-packages">
         {
-          other_packages.map((pkg, index) => {
-            return <Card key={index} {...pkg} />
-          })
+          <Card {...other_packages[showIndex]} showIndex={showIndex} setShowIndex={setShowIndex} other_packages={other_packages} />
         }
     </div>
   )
--- src/data/other_packages.js
+++ src/data/other_packages.js
@@ -6,6 +6,38 @@ const other_packages = [
         enrolled: "1.2k",
         rating: 4.7,
         reviews: 12340
+    },
+    {
+        image: "assets/course_image.svg",
+        title: "Gate XE Rotation",
+        topics: ["General Aptitude","Fluid Mechanics","Food Mechanics","Engeneering Mathematics"],
+        enrolled: "1.2k",
+        rating: 4.7,
+        reviews: 12340
+    },
+    {
+        image: "assets/course_image.svg",
+        title: "Gate XE Gas Theory",
+        topics: ["General Aptitude","Fluid Mechanics","Food Mechanics","Engeneering Mathematics"],
+        enrolled: "1.2k",
+        rating: 4.7,
+        reviews: 12340
+    },
+    {
+        image: "assets/course_image.svg",
+        title: "Gate XE Mathematics",
+        topics: ["General Aptitude","Fluid Mechanics","Food Mechanics","Engeneering Mathematics"],
+        enrolled: "1.2k",
+        rating: 4.7,
+        reviews: 12340
+    },
+    {
+        image: "assets/course_image.svg",
+        title: "Gate XE Chemistry",
+        topics: ["General Aptitude","Fluid Mechanics","Food Mechanics","Engeneering Mathematics"],
+        enrolled: "1.2k",
+        rating: 4.7,
+        reviews: 12340
     }
 ];
 
